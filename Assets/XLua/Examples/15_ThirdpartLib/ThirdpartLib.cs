a69ead22814cf16002cb18fe8be535078f8c8f5d
synchronized <synchronized@yeah.net>
2024-09-13 21:52:36 +0800

    增加第三方库的例子

---
+++ Assets/XLua/Examples/15_ThirdpartLib/ThirdpartLib.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using XLua;
+using XLua.LuaDLL;
+
+namespace XLuaTest
+{
+
+    public class ThirdpartLib : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            LuaEnv luaenv = new LuaEnv();
+            luaenv.AddBuildin("cjson", Lua.LoadCjson);
+            luaenv.AddBuildin("cjson.safe", Lua.LoadCjsonSafe);
+            luaenv.AddBuildin("lpeg", Lua.LoadLpeg);
+
+            luaenv.AddBuildin("pb", Lua.LoadPb);
+            luaenv.AddBuildin("pb.io", Lua.LoadPbIo);
+            luaenv.AddBuildin("pb.conv", Lua.LoadPbConv);
+            luaenv.AddBuildin("pb.buffer", Lua.LoadPbBuffer);
+            luaenv.AddBuildin("pb.slice", Lua.LoadPbSlice);
+            luaenv.AddBuildin("pb.unsafe", Lua.LoadPbUnsafe);
+
+            luaenv.AddBuildin("rapidjson", Lua.LoadRapidJson);
+            luaenv.AddBuildin("crypt", Lua.LoadCrypt);
+            luaenv.AddBuildin("sproto.core", Lua.LoadSprotoCore);
+
+            luaenv.DoString("require 'cjson'");
+            luaenv.DoString("require 'cjson.safe'");
+            luaenv.DoString("require 'lpeg'");
+
+            luaenv.DoString("require 'pb'");
+            luaenv.DoString("require 'pb.io'");
+            luaenv.DoString("require 'pb.conv'");
+            luaenv.DoString("require 'pb.buffer'");
+            luaenv.DoString("require 'pb.slice'");
+            luaenv.DoString("require 'pb.unsafe'");
+
+            luaenv.DoString("require 'rapidjson'");
+            luaenv.DoString("require 'crypt'");
+            luaenv.DoString("require 'sproto.core'");
+            luaenv.Dispose();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+
+}
